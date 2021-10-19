a56bb6b75324bed55f061c8d87631eaeed55683d
MattHenao <matthenao10@gmail.com>
2021-10-19 11:27:37 -0500

    Argumentos Call

---
+++ 08. Objetos en JavaScript/08-14-ParametrosYCall-UJS.js
@@ -0,0 +1,17 @@
+//* Pasar argumentos con call()
+let persona1 = {
+    nombre:'Juan',
+    apellido:'Perez',
+    // Se pasan los argumentos
+    nombreCompleto: function(titulo, tel){
+        return titulo + ': ' + this.nombre + ' ' + this.apellido + ' ' + tel;
+    }
+}
+
+let persona2 = {
+    nombre: 'Carlos',
+    apellido:'Lara'
+}
+
+// Se llaman los valores de los argumentos
+console.log(persona1.nombreCompleto.call(persona2, 'Ing', '5552728'));
