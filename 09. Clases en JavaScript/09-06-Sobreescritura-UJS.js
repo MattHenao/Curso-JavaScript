08cf61766e6c2ae3e55c3d48b17c32f402ce115f
MattHenao <70972324+MattHenao@users.noreply.github.com>
2021-12-02 08:17:45 -0500

    Sobreescritura JS

---
+++ 09. Clases en JavaScript/09-06-Sobreescritura-UJS.js
@@ -0,0 +1,49 @@
+//* Sobreescritura de metodos
+
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+    get nombre(){
+        return this._nombre;
+    }
+    set nombre(nombre){
+        this._nombre = nombre;
+    }
+
+    get apellido(){
+        return this._apellido;
+    }
+    set apellido(apellido){
+        this._apellido = apellido;
+    }
+
+    nombreCompleto(){
+        return this._nombre + ' ' + this._apellido
+    }
+}
+
+class Empleado extends Persona{
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido); // Llama los atributos de la clase padre
+        this._departamento = departamento;
+        }
+
+    get departamento(){
+        return this._departamento;
+    }
+
+    set departamento(departamento){
+        this._departamento = departamento;
+    }
+
+    // Sobreescritura
+    nombreCompleto(){
+        return super.nombreCompleto() + ', ' + this._departamento;
+    }
+}
+
+let empleado1 = new Empleado ('Mateo', 'Henao', 'Sistemas');
+console.log(empleado1.nombreCompleto());
