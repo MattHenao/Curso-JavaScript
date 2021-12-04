e059c2a7b75bbffe40f8c47b9e240be727b77574
MattHenao <70972324+MattHenao@users.noreply.github.com>
2021-12-04 10:22:16 -0500

    Modo strict

---
+++ 10. Modo strict/10-01-ModoStrict-UJS.js
@@ -0,0 +1,16 @@
+//* Modo strict
+'use strict';
+
+//x = 10; no va a servir
+let x = 10; // Si sirve
+console.log(x);
+
+miFuncion();
+
+function miFuncion(){
+    //Aplica para todo el proyecto
+    //* 'use strict';
+    //y = 3;
+    let y = 3;
+    console.log(y);
+}
